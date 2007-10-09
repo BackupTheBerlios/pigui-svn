da3f6ac5a05811b1809f90462fc5a4c268edd249
punto <punto@8c074b69-ad3b-0410-a872-b0e6b17f3a32>
2007-10-09 22:21:56 +0000

    adding color picker

--- trunk/gui/bundles/color_picker.cpp
+++ trunk/gui/bundles/color_picker.cpp
@@ -11,8 +11,103 @@
 //
 #include "color_picker.h"
 
-ColorPicker::ColorPicker()
-{
+#include "base/painter.h"
+#include "containers/grid_container.h"
+#include "widgets/label.h"
+
+namespace GUI {
+
+void ColorPicker::ColorShow::draw(const Point& p_global,const Size& p_size,const Rect& p_exposed) {
+	
+	get_painter()->draw_fill_rect(Point(),p_size,color);
+}
+	
+void ColorPicker::set_color(const Color &p_color, int p_alpha) {
+	
+	color=p_color;
+	if (p_alpha>=0 || p_alpha<256) {
+		
+		alpha=p_alpha;
+	}
+	show_color->set_color(color);
+	
+	updating=true;
+	
+	r_slider->get_range()->set( p_color.r );
+	g_slider->get_range()->set( p_color.g );
+	b_slider->get_range()->set( p_color.b );
+	if (a_slider)
+		a_slider->get_range()->set( alpha );
+			
+	updating=false;
+	
+}
+Color ColorPicker::get_color() const {
+	
+	return color;	
+}
+Uint8 ColorPicker::get_alpha() const {
+	
+	return alpha;
+}
+	
+void ColorPicker::color_changed_callback(double) {
+		
+	if (updating)
+		return;
+	
+	
+	color.r=(Uint8)r_slider->get_range()->get();
+	color.g=(Uint8)g_slider->get_range()->get();
+	color.b=(Uint8)b_slider->get_range()->get();
+	if (!use_alpha)
+		set_color(color);
+	else {
+		alpha=(Uint8)a_slider->get_range()->get();
+		set_color(color,alpha);
+	}
+
+	color_changed_signal.call();
+	
+}
+	
+ColorPicker::ColorPicker(bool p_with_alpha) {
+	
+	use_alpha=p_with_alpha;
+	
+	show_color = add( new ColorShow, 1 );
+	
+	GridContainer *g = add( new GridContainer, 2 );
+	
+	g->add( new Label("R"), false, false );
+	r_slider = g->add( new HSlider, true, false );
+	r_slider->get_range()->config( 0, 255, 0, 1 );
+	r_slider->get_range()->value_changed_signal.connect( this, &ColorPicker::color_changed_callback );
+	
+	g->add( new Label("G"), false, false );
+	g_slider = g->add( new HSlider, true, false );
+	g_slider->get_range()->config( 0, 255, 0, 1 );
+	g_slider->get_range()->value_changed_signal.connect( this, &ColorPicker::color_changed_callback );
+	
+	g->add( new Label("B"), false, false );
+	b_slider = g->add( new HSlider, true, false );
+	b_slider->get_range()->config( 0, 255, 0, 1 );
+	b_slider->get_range()->value_changed_signal.connect( this, &ColorPicker::color_changed_callback );
+	
+	if (use_alpha) {
+		g->add( new Label("A"), false, false );
+		a_slider = g->add( new HSlider, true, false );
+		a_slider->get_range()->config( 0, 255, 0, 1 );
+		a_slider->get_range()->value_changed_signal.connect( this, &ColorPicker::color_changed_callback );
+	} else {
+		
+		a_slider=0;
+	}
+	
+	
+	set_color(Color(0),255);
+	updating=false;
+	
 }
 
 
@@ -21,3 +116,4 @@ ColorPicker::~ColorPicker()
 }
 
 
+}

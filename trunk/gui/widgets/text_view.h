607cd9958121f88fe66f09a12037260e233f2afb
ivodo <ivodo@8c074b69-ad3b-0410-a872-b0e6b17f3a32>
2008-05-28 21:15:05 +0000

    fixed little bug

--- trunk/gui/widgets/text_view.h
+++ trunk/gui/widgets/text_view.h
@@ -2,7 +2,7 @@
 //
 // C++ Interface: label
 //
-// Description: 
+// Description:
 //
 //
 // Author: Juan Linietsky <reduzio@gmail.com>, (C) 2006
@@ -23,10 +23,10 @@ namespace GUI {
 	@author Juan Linietsky <reduzio@gmail.com>
 */
 class TextView : public RangeOwner {
-public:	
-	
+public:
+
 	enum Align {
-		
+
 		ALIGN_LEFT,
 		ALIGN_CENTER,
 		ALIGN_RIGHT,
@@ -47,29 +47,30 @@ private:
 		WordCache *next;
 		WordCache() { char_pos=0; word_len=0; pixel_width=0; next=0; }
 	};
-	
-	
+
+
+	bool word_cache_dirty;
 	void regenerate_word_cache();
-	
+
 	WordCache *word_cache;
 
-	virtual void resize(const Size& p_new_size); 
+	virtual void resize(const Size& p_new_size);
 	virtual Size get_minimum_size_internal();
 protected:
 
-	virtual String get_type();	
+	virtual String get_type();
 public:
-	
+
 	//void mouse_enter() { update(); } for testing
-	
+
 	void draw(const Point& p_pos,const Size& p_size,const Rect& p_exposed);
-	
+
 	void set_align(Align p_align);
 	Align get_align();
-	
+
 	void set_text(const String& p_string);
 	String get_text();
-	
+
 	TextView(String p_text="",Align p_align=ALIGN_LEFT);
 
     ~TextView();

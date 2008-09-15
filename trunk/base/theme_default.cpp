33be6a9f0e28bb3b05a761e8e68a6b10b79c85de
reduz <reduz@8c074b69-ad3b-0410-a872-b0e6b17f3a32>
2008-09-15 16:08:44 +0000

    missing

---
+++ trunk/base/theme_default.cpp
@@ -0,0 +1,32 @@
+//
+// C++ Implementation: theme_default
+//
+// Description: 
+//
+//
+// Author: Juan Linietsky <reduzio@gmail.com>, (C) 2008
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+#include "theme_default.h"
+
+namespace GUI {
+
+
+void ThemeDefault::setup( Theme * p_theme ) {
+
+
+}
+
+ThemeDefault::ThemeDefault()
+{
+}
+
+
+ThemeDefault::~ThemeDefault()
+{
+}
+
+
+}
---
+++ trunk/base/theme_default.h
@@ -0,0 +1,35 @@
+//
+// C++ Interface: theme_default
+//
+// Description: 
+//
+//
+// Author: Juan Linietsky <reduzio@gmail.com>, (C) 2008
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+#ifndef GUITHEME_DEFAULT_H
+#define GUITHEME_DEFAULT_H
+
+
+#include "base/theme.h"
+
+namespace GUI {
+
+/**
+	@author Juan Linietsky <reduzio@gmail.com>
+*/
+class ThemeDefault{
+public:
+
+	void setup( Theme * p_theme );
+	
+	ThemeDefault();	
+	~ThemeDefault();
+
+};
+
+}
+
+#endif
